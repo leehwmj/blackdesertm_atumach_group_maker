b822d440a16df1db16f74e87acf8c6e5b6ab133d
leehwmj <hyunwoo@Hyunwoos-MacBook-Air.local>
2022-03-21 22:01:37 +0900

    object test

---
+++ app_obj.js
@@ -0,0 +1,35 @@
+// var att_obj = {p01 : 430,
+//     p02 : 380,
+//     p03 : 330,
+//     p04 : 390,
+//     p05 : 400, 
+//     p06 : 370,
+//     p07 : 300,
+//     p08 : 350,
+//     p09 : 360,
+//     p10 : 330,
+//     p11 : 355,
+//     p12 : 358,
+//     p13 : 338,
+//     p14 : 435,
+//     p15 : 470
+//     };
+
+    // console.log(att_obj.p01)
+
+att_list_obj = [
+    {
+        id : 'hachuping',
+        name : '하츄핑',
+        att : 430,
+        day : 1
+    },
+    {
+        id : 'asdf',
+        name : '기원',
+        att : 380,
+        day : 1
+    }
+]
+
+console.log(att_list_obj)
